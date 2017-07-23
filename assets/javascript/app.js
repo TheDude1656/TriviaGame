55084932f23cb5d7f977a72b7cd0ffa97d40704f
Kyle Atchison <katchison@gmail.com>
2017-07-23 00:03:01 -0500

    sat-midnight

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,52 @@
-$("#clickme").click(function() {
-    $(this).remove();
+var questionsAnswers = [];
 
-});
+$(function() {
+    $("#options").hide();
+    $("#tLeft").hide();
+    $("#clickme").click(function() {
+        $(this).remove();
+        $("#options").show();
+        $("#tLeft").show();
+        questions();
+    });
 
 
-$.ajax({
-    url: "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple",
-    method: "GET"
-}).done(function(response) {
-    console.log(response)
+    $.ajax({
+        url: "https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple",
+        method: "GET"
+    }).done(function(response) {
+        questionsAnswers.push(response);
+        console.log(response)
+    });
+
+    function questions() {
+        $("#question").html(questionsAnswers["0"].results["0"].question);
+        $("#opt1").html(questionsAnswers["0"].results["0"].correct_answer);
+        $("#opt2").html(questionsAnswers["0"].results["0"].incorrect_answers["0"]);
+        $("#opt3").html(questionsAnswers["0"].results["0"].incorrect_answers["1"]);
+        $("#opt4").html(questionsAnswers["0"].results["0"].incorrect_answers["2"]);
+    }
+
+    function startTimer(duration, display) {
+        var timer = duration,
+            seconds;
+        setInterval(function() {
+
+            seconds = parseInt(timer % 60, 10);
+
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.text("Time Remaining: " + seconds);
+
+            if (--timer < 0) {
+
+                timer = duration;
+
+            }
+        }, 1000);
+    }
+    jQuery(function($) {
+        display = $("#timer");
+        startTimer(15, display);
+    })
 });
